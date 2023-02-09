a2964c8925be02d8a153be3a6b9b5401339edb44
unknown <martin.scheidt1999@web.de>
2023-02-09 21:41:46 +0100

    init

--- umappy.py
+++ umappy.py
@@ -1,67 +1,85 @@
 import numpy as np
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+# from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import umap
+import sklearn.cluster as cluster
+from sklearn.metrics import adjusted_rand_score, adjusted_mutual_info_score
 # %matplotlib inline
-sns.set_theme(style='dark', context='notebook', rc={'figure.figsize':(14,10)})
+sns.set(style='white',
+# context='notebook',
+rc={'figure.figsize':(10,8)})
 
 test = pd.read_csv("./Martin2.csv", sep=";", decimal=",", usecols=['maxdV', 'type'])[['type', 'maxdV']]
 test.head()
 print(test)
-
 test = test.dropna()
-# test = test.drop('type')
-test.type.value_counts()
 
-sns.pairplot(test, hue='type')
-# sns.clustermap(test, hue='type')
+standard_embedding = umap.UMAP(
 
-reducer = umap.UMAP()
+).fit_transform(test)
 
-test_data = test[
-    [
-        "maxdV",
-        # "type",
-        # "rheobase",
-        # # "Name",
-        # "Vh",
-        # # "Rin",
-        # "Tau",
-        # "Cm",
-        # "Rin",
-        # "Thres",
-        # # "Threshold_LP",
-        # "Thres_Time",
-        # "AP_duration",
-        # # "Rise",
-        # "ADP",
-        # "ADP_area",
-        # # "FAHP",
-        # # "FAHP_time",
+plt.scatter(standard_embedding[:, 0], standard_embedding[:, 1], s=0.1, cmap='spectral')
 
-    ]
-].values
-scaled_test_data = StandardScaler().fit_transform(test_data)
+# test = test.dropna()
+# # test = test.drop('type')
+# test.type.value_counts()
 
-print(scaled_test_data)
+# sns.pairplot(test, hue='type')
 
-embedding = reducer.fit_transform(scaled_test_data)
-embedding.shape
+# # sns.clustermap(test, hue='type')
 
-print("###################################")
-print(embedding)
 
-print(test_data)
-plt.scatter(
-    embedding[:, 0],
-    embedding[:, 1],
-    c=[sns.color_palette()[x] for x in test.type.map({"Hetero":1, "Wt":2})])
-plt.gca().set_aspect('equal', 'datalim')
-plt.title('UMAP mit Chiaras Daten', fontsize=24);
+
+
+# reducer = umap.UMAP()
+
+# test_data = test[
+#     [
+#         "maxdV",
+#         # "type",
+#         # "rheobase",
+#         # # "Name",
+#         # "Vh",
+#         # # "Rin",
+#         # "Tau",
+#         # "Cm",
+#         # "Rin",
+#         # "Thres",
+#         # # "Threshold_LP",
+#         # "Thres_Time",
+#         # "AP_duration",
+#         # # "Rise",
+#         # "ADP",
+#         # "ADP_area",
+#         # # "FAHP",
+#         # # "FAHP_time",
+
+#     ]
+# ].values
+# scaled_test_data = umap.UMAP(
+#     # n_neighbors=30,
+#     # min_dist=0.0,
+#     # n_components=2,
+#     # random_state=42,
+# ).fit_transform(test_data)
+
+# print(scaled_test_data)
+
+# embedding = reducer.fit_transform(scaled_test_data)
+# embedding.shape
+
+# print("###################################")
+# print(embedding)
+
+# print(test_data)
+# plt.scatter(scaled_test_data[:, 0], scaled_test_data[:, 1],
+# c=sns.color_palette(), s=0.1, cmap='Spectral')
+# plt.gca().set_aspect('equal', 'datalim')
+# plt.title('UMAP mit Chiaras Daten', fontsize=24);
 
 plt.show()
 
